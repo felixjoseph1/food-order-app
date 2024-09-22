dabbe26dcd54aaee501a0e83b81c6ea45febe6a8
Programmer <Programmer@example.com>
2024-09-22 20:02:36 +0530

    url changed

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,8 +5,8 @@ import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = ()=> {
-    const [resList,setresList] = useState(restaurantList); //use restaurantList for initial state 
-    const [filteredData,setFilteredData] = useState(restaurantList);
+    const [resList,setresList] = useState([]); //use restaurantList for initial state 
+    const [filteredData,setFilteredData] = useState([]);
     const [search,setsearch] = useState("");
 
     useEffect(()=>{
@@ -15,16 +15,21 @@ const Body = ()=> {
 
     const fetchData=async()=>{
         const data=await fetch(
-            'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING%20Read'
-            ,{cache: "no-store"}
+            // 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING%20Read'
+            'https://www.swiggy.com/mapi/homepage/getCards?lat=13.0849557&lng=80.2101342'
             
         );
         
         const resData=await data.json();
-        //console.log(resData);
-        //console.log(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info.name);
-        setresList(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredData(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        // console.log(resData);
+        // console.log(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        // setresList(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        // setFilteredData(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+        console.log(resData);
+        console.log(resData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setresList(resData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredData(resData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
     }
 
 
